0874258280572f127703808b15b63ac05b285951
Guillermo Ampie <ga.ampie@gmail.com>
2016-01-18 01:39:16 -0430

    Add Schematic support

---
+++ app/src/main/java/com/ampie_guillermo/popularmovies/database/MoviesDatabase.java
@@ -0,0 +1,23 @@
+package com.ampie_guillermo.popularmovies.database;
+
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
+/**
+ * Database & Content Provider support with Schematic
+ * https://github.com/SimonVT/schematic.git - Automatically generate a
+ * ContentProvider backed by an SQLite database.
+ */
+
+@Database(version = MoviesDatabase.VERSION)
+public final class MoviesDatabase {
+
+    public static final int VERSION = 1;
+
+    private MoviesDatabase() {
+    }
+
+    @Table(MovieColumns.class)
+    public static final String MOVIES_DB = "moviesDB";
+
+}
---
+++ app/src/main/java/com/ampie_guillermo/popularmovies/database/MoviesProvider.java
@@ -0,0 +1,53 @@
+package com.ampie_guillermo.popularmovies.database;
+
+import android.net.Uri;
+
+import net.simonvt.schematic.annotation.ContentProvider;
+import net.simonvt.schematic.annotation.ContentUri;
+import net.simonvt.schematic.annotation.InexactContentUri;
+import net.simonvt.schematic.annotation.TableEndpoint;
+
+/**
+ * Database & Content Provider support with Schematic
+ * https://github.com/SimonVT/schematic.git - Automatically generate a
+ * ContentProvider backed by an SQLite database.
+ */
+
+@ContentProvider(authority = MoviesProvider.AUTHORITY, database = MoviesDatabase.class)
+public final class MoviesProvider {
+
+    public MoviesProvider() {
+    }
+
+    public static final String AUTHORITY = "com.ampie_guillermo.popularmovies.MoviesProvider";
+
+    static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    interface Path {
+        String MOVIES = "movies";
+    }
+
+    private static Uri buildUri(String... paths) {
+        Uri.Builder builder = BASE_CONTENT_URI.buildUpon();
+        for (String path : paths) {
+            builder.appendPath(path);
+        }
+        return builder.build();
+    }
+
+    @TableEndpoint(table = MoviesDatabase.MOVIES_DB) public static class Movies {
+
+        @ContentUri(path = Path.MOVIES,
+                    type = "vnd.android.cursor.dir/movie")
+        public static final Uri CONTENT_URI = buildUri(Path.MOVIES);
+
+        @InexactContentUri(name = "MOVIE_ID",
+                           path = Path.MOVIES + "/#",
+                           type = "vnd.android.cursor.item/movie",
+                           whereColumn = MovieColumns.ID,
+                           pathSegment = 1)
+        public static Uri withId(long id){
+            return buildUri(Path.MOVIES, String.valueOf(id));
+        }
+    }
+}
