7b3d46eaff4ad964beb4c27ec013b752e15d1923
Guillermo Ampie <ga.ampie@gmail.com>
2018-06-26 21:03:35 -0500

    Move inner class MovieViewHolder to first level class

--- app/src/main/java/com/ampie_guillermo/popularmovies/ui/adapter/MovieAdapter.java
+++ app/src/main/java/com/ampie_guillermo/popularmovies/ui/adapter/MovieAdapter.java
@@ -1,19 +1,13 @@
 package com.ampie_guillermo.popularmovies.ui.adapter;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.support.v7.util.DiffUtil;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ListView;
 import com.ampie_guillermo.popularmovies.R;
 import com.ampie_guillermo.popularmovies.model.Movie;
-import com.ampie_guillermo.popularmovies.ui.adapter.MovieAdapter.MovieViewHolder;
-import com.ampie_guillermo.popularmovies.utils.DrawablePlaceholderSingleton;
-import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -119,36 +113,4 @@ public class MovieAdapter extends ListAdapter<Movie, MovieViewHolder> {
     void onMovieItemClick(int clickedItemIndex);
   }
 
-  // The View Holder used for each movie poster
-  static class MovieViewHolder extends RecyclerView.ViewHolder {
-    private final Drawable drawablePlaceholder;
-    private final Drawable drawableErrorPlaceHolder;
-    private final MovieItemClickListener onClickListener;
-    private final ImageView movieThumbnailView;
-
-    MovieViewHolder(final View view, final MovieItemClickListener onItemClickListener) {
-      super(view);
-
-      final DrawablePlaceholderSingleton placeholders =
-          DrawablePlaceholderSingleton.getInstance(itemView.getResources());
-
-      drawablePlaceholder = placeholders.getDrawablePlaceHolder();
-      drawableErrorPlaceHolder = placeholders.getDrawablePlaceHolder();
-      movieThumbnailView = view.findViewById(R.id.image_movie_poster_movie_poster);
-
-      onClickListener = onItemClickListener;
-      movieThumbnailView.setOnClickListener(
-          v -> onClickListener.onMovieItemClick(getAdapterPosition()));
-    }
-
-    void setupItemView(final Movie currentMovie) {
-      // See comment in DrawablePlaceholderSingleton() to allow vector drawables in
-      // API level < 21 (Lollipop)
-      Picasso.get()
-          .load(currentMovie.getPosterUri())
-          .placeholder(drawablePlaceholder)
-          .error(drawableErrorPlaceHolder)
-          .into(movieThumbnailView);
-    }
-  }
 }
