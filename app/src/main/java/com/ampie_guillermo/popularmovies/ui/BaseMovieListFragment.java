f77cbc2216e10f1b52e56bd9e5b746618c72b173
Guillermo Ampie <ga.ampie@gmail.com>
2018-06-26 21:18:22 -0500

    Change class hierarchy from: MovieListFragment |--- PopularMovieListFragment |--- RatedMovieListFragment |--- FavouriteMovieListFragment
    to:
    BaseMovieListFragment
    |--- FavouriteMovieListFragment
    |--- MovieListFragment
         |--- PopularMovieListFragment
         |--- RatedMovieListFragment

---
+++ app/src/main/java/com/ampie_guillermo/popularmovies/ui/BaseMovieListFragment.java
@@ -0,0 +1,273 @@
+package com.ampie_guillermo.popularmovies.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import com.ampie_guillermo.popularmovies.R;
+import com.ampie_guillermo.popularmovies.model.Movie;
+import com.ampie_guillermo.popularmovies.ui.adapter.MovieAdapter;
+import com.ampie_guillermo.popularmovies.utils.MyPMErrorUtils;
+
+/**
+ * NOTE: MovieDB API Key stored in "~/.gradle/gradle.properties" using the method specified for
+ * Sunshine-Version-2 with API key
+ */
+public class BaseMovieListFragment
+    extends Fragment
+    implements MovieAdapter.MovieItemClickListener {
+
+  protected static final String EXTRA_MOVIE_SORTING_METHOD = "EXTRA_MOVIE_SORTING_METHOD";
+  private static final String LOG_TAG = BaseMovieListFragment.class.getSimpleName();
+
+  protected RecyclerView mRvMovieGrid;
+
+  private ProgressBar mLoadingIndicator;
+  private ImageView mImageCloudOff;
+  //  private MovieAdapter mMovieAdapter;
+  private String mSortingMethodParam;
+  private FetchMoviesListener moviesFetchListener;
+
+  public BaseMovieListFragment() {
+  }
+
+  /**
+   * Called to ask the fragment to save its current dynamic state, so it
+   * can later be reconstructed in a new instance of its process is
+   * restarted.  If a new instance of the fragment later needs to be
+   * created, the data you place in the Bundle here will be available
+   * in the Bundle given to {@link #onCreate(Bundle)},
+   * #onCreateView(LayoutInflater, ViewGroup, Bundle)}, and
+   * {@link #onActivityCreated(Bundle)}.
+   * This corresponds to  Activity onSaveInstanceState(Bundle)
+   * Activity onSaveInstanceState(Bundle)} and most of the discussion there
+   * applies here as well.  Note however: <em>this method may be called
+   * at any time before {@link #onDestroy()}</em>.  There are many situations
+   * where a fragment may be mostly torn down (such as when placed on the
+   * back stack with no UI showing), but its state will not be saved until
+   * its owning activity actually needs to save its state.
+   *
+   * @param outState Bundle in which to place your saved state.
+   */
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+    /**
+     * There is no need to use savedInstanceState because mCachedMovieList is managed by the
+     * AsyncTaskLoader on configuration changes
+     */
+//    if (!mCachedMovieList.isEmpty()) {
+//      outState.putParcelableArrayList(MOVIE_LIST, mCachedMovieList);
+//    }
+  }
+
+  /**
+   * Called to do initial creation of a fragment.  This is called after
+   * {@link #onAttach (Activity)} and before
+   * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+   * Note that this can be called while the fragment's activity is
+   * still in the process of being created.  As such, you can not rely
+   * on things like the activity's content divider_view_1 hierarchy being initialized
+   * at this point.  If you want to do work once the activity itself is
+   * created, see {@link #onActivityCreated(Bundle)}.
+   *
+   * @param savedInstanceState If the fragment is being re-created from a previous saved state, this
+   * is the state.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    /**
+     * There is no need to use savedInstanceState() because the movie list fetched is persisted
+     * / cached by the AsyncTaskLoader's subclass MovieListLoader upon configuration changes
+     */
+//    if ((savedInstanceState != null) && savedInstanceState.containsKey(MOVIE_LIST)) {
+//      // Let's get the saved movie list array from a saved state
+//      mCachedMovieList = savedInstanceState.getParcelableArrayList(MOVIE_LIST);
+//    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+    mImageCloudOff = rootView.findViewById(R.id.image_main_no_connection);
+    mLoadingIndicator = rootView.findViewById(R.id.progressbar_main_base);
+    showLoadingIndicator();
+
+    // Get a reference to the RecyclerView (the movie grid), and attach the movie adapter to it.
+    mRvMovieGrid = rootView.findViewById(R.id.recycler_main_movie_grid);
+
+    // Al the items (movie posters) in the RecyclerView are the same size
+    mRvMovieGrid.setHasFixedSize(true);
+
+    // We will show the movie list in a grid with a parameterized number of columns
+    mRvMovieGrid.setLayoutManager(new GridLayoutManager(getContext(),
+        getResources().getInteger(R.integer.num_columns)));
+
+//    // Set an empty adapter because the movies have not been fetched yet
+//    mMovieAdapter = new MovieAdapter(this);
+//    mRvMovieGrid.setAdapter(mMovieAdapter);
+//
+    /**
+     * The RecyclerView's Adapter will take data from a a JSON response from TheMovieDB.org
+     * server / local DB
+     */
+    moviesFetchListener.setupRecyclerViewAdapter();
+    return rootView;
+  }
+
+  /**
+   * Called when the fragment's activity has been created and this
+   * fragment's hierarchy instantiated.  It can be used to do final
+   * initialization once these pieces are in place, such as retrieving
+   * views or restoring state.  It is also useful for fragments that use
+   * {@link #setRetainInstance(boolean)} to retain their instance,
+   * as this callback tells the fragment when it is fully associated with
+   * the new activity instance.  This is called after {@link #onCreateView}
+   * and before {@link #onViewStateRestored(Bundle)}.
+   *
+   * @param savedInstanceState If the fragment is being re-created from a previous saved state, this
+   * is the state.
+   */
+  @Override
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    moviesFetchListener.getMovies();
+  }
+
+  @Override
+  public void onMovieItemClick(int clickedItemIndex) {
+    MyPMErrorUtils.validateIndexInCollection(clickedItemIndex, moviesFetchListener.movieListSize());
+//    final Movie currentMovie = mMovieList.get(clickedItemIndex);
+    final Movie currentMovie = moviesFetchListener.getSelectedMovie(clickedItemIndex);
+    final Intent intent = new Intent(getActivity(),
+        MovieDetailActivity.class).putExtra(getString(R.string.EXTRA_SELECTED_MOVIE), currentMovie);
+
+    startActivity(intent);
+  }
+
+  protected String getSortingMethodParam() {
+    return mSortingMethodParam;
+  }
+
+  protected void setSortingMethodParam(String sortingMethodParam) {
+    mSortingMethodParam = sortingMethodParam;
+  }
+
+  protected void showLoadingIndicator() {
+    mLoadingIndicator.setVisibility(View.VISIBLE);
+  }
+
+  protected void hideLoadingIndicator() {
+    mLoadingIndicator.setVisibility(View.INVISIBLE);
+  }
+
+  protected void showErrorDisplay() {
+    mRvMovieGrid.setVisibility(View.INVISIBLE);
+    mImageCloudOff.setVisibility(View.VISIBLE);
+  }
+
+  protected final void registerFetchMoviesListener(final FetchMoviesListener listener) {
+    moviesFetchListener = listener;
+  }
+
+  public interface FetchMoviesListener {
+
+    void getMovies();
+
+    Movie getSelectedMovie(int clickedItemIndex);
+
+    int movieListSize();
+
+    /*
+     * Create & attach the RecyclerView Adapter
+     */
+    void setupRecyclerViewAdapter();
+
+  }
+
+  public static class FavouriteMovieListFragment
+      extends BaseMovieListFragment
+      implements FetchMoviesListener {
+
+    // FavouriteMovieAdapter mMovieAdapter;
+
+    public FavouriteMovieListFragment() {
+      /*
+       * We do not need to call setSortingMethodParam(). In this case, we are going to read
+       * from the local database and not to connect to TheMovieDB server
+       */
+      registerFetchMoviesListener(this);
+    }
+
+    @Override
+    public void getMovies() {
+      /*
+       * READ FROM DATABASE AND POPULATE "mCachedMovieList":
+       * if (Database is empty) {
+       *   show "No favorite movies"
+       * }
+       * else {
+       * display all movies in the database (we are storing only the FAVORITES movies
+       * in the database)
+       * }
+       */
+    }
+
+    @Override
+    public Movie getSelectedMovie(int clickedItemIndex) {
+      /*
+       * Get Movie from Cursor
+       */
+      assert 1 != 1;
+      return null;
+    }
+
+    @Override
+    public int movieListSize() {
+      /*
+       * Get size from Cursor
+       */
+      assert 1 != 1;
+      return 0;
+    }
+
+    @Override
+    public void setupRecyclerViewAdapter() {
+      // Set an empty adapter because the movies have not been fetched yet
+//      mMovieAdapter = new FavouriteMovieAdapter(this);
+//      mRvMovieGrid.setAdapter(mMovieAdapter);
+      assert 1 != 1;
+    }
+
+    /**
+     * Called when the fragment is visible to the user and actively running.
+     * This is generally
+     * tied to {@link Activity#onResume() Activity.onResume} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onResume() {
+      super.onResume();
+      // Re-query the DB after the Fragment/Activity has been -paused-: Any time we leave the
+      // MainActivity / BaseMovieListFragment and return(when we go and get back from
+      // MovieDetailActivity / MovieDetailFragment where the selected movie could have been marked
+      // as favourite / not favourite
+
+      // restart the loader!
+
+    }
+  }
+}
--- app/src/main/java/com/ampie_guillermo/popularmovies/ui/MovieListFragment.java
+++ app/src/main/java/com/ampie_guillermo/popularmovies/ui/MovieListFragment.java
@@ -1,30 +1,21 @@
 package com.ampie_guillermo.popularmovies.ui;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.CursorLoader;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.CursorAdapter;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 import com.ampie_guillermo.popularmovies.BuildConfig;
 import com.ampie_guillermo.popularmovies.R;
 import com.ampie_guillermo.popularmovies.model.Movie;
+import com.ampie_guillermo.popularmovies.ui.BaseMovieListFragment.FetchMoviesListener;
 import com.ampie_guillermo.popularmovies.ui.adapter.MovieAdapter;
 import com.ampie_guillermo.popularmovies.utils.MyPMErrorUtils;
 import com.ampie_guillermo.popularmovies.utils.UiErrorHelper;
@@ -43,169 +34,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * NOTE: MovieDB API Key stored in "~/.gradle/gradle.properties" using the method specified for
- * Sunshine-Version-2 with API key
- */
 public class MovieListFragment
-    extends Fragment
+    extends BaseMovieListFragment
     implements LoaderManager.LoaderCallbacks<ArrayList<Movie>>,
-    MovieAdapter.MovieItemClickListener {
+    FetchMoviesListener {
 
-  private static final int MOVIE_LIST_LOADER_ID = 1000;
-  private static final String EXTRA_MOVIE_SORTING_METHOD = "EXTRA_MOVIE_SORTING_METHOD";
-  private static final String LOG_TAG = MovieListFragment.class.getSimpleName();
+  private static final String LOG_TAG = com.ampie_guillermo.popularmovies.ui.MovieListFragment.class.getSimpleName();
   private static final int MOVIES_RESULT_SIZE = 20;
-
-  private ProgressBar mLoadingIndicator;
-  private ImageView mImageCloudOff;
-  private RecyclerView mRvMovieGrid;
-  private MovieAdapter mMovieAdapter;
-  private String mSortingMethodParam;
+  private static final int MOVIE_LIST_LOADER_ID = 1000;
   private ArrayList<Movie> mMovieList;
+  private MovieAdapter mMovieAdapter;
 
   public MovieListFragment() {
     mMovieList = new ArrayList<>(MOVIES_RESULT_SIZE);
+    registerFetchMoviesListener(this);
   }
 
-  /**
-   * Called to ask the fragment to save its current dynamic state, so it
-   * can later be reconstructed in a new instance of its process is
-   * restarted.  If a new instance of the fragment later needs to be
-   * created, the data you place in the Bundle here will be available
-   * in the Bundle given to {@link #onCreate(Bundle)},
-   * #onCreateView(LayoutInflater, ViewGroup, Bundle)}, and
-   * {@link #onActivityCreated(Bundle)}.
-   * This corresponds to  Activity onSaveInstanceState(Bundle)
-   * Activity onSaveInstanceState(Bundle)} and most of the discussion there
-   * applies here as well.  Note however: <em>this method may be called
-   * at any time before {@link #onDestroy()}</em>.  There are many situations
-   * where a fragment may be mostly torn down (such as when placed on the
-   * back stack with no UI showing), but its state will not be saved until
-   * its owning activity actually needs to save its state.
-   *
-   * @param outState Bundle in which to place your saved state.
-   */
-  @Override
-  public void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-
-    /**
-     * There is no need to use savedInstanceState because mCachedMovieList is managed by the
-     * AsyncTaskLoader on configuration changes
-     */
-//    if (!mCachedMovieList.isEmpty()) {
-//      outState.putParcelableArrayList(MOVIE_LIST, mCachedMovieList);
-//    }
-  }
-
-  /**
-   * Called to do initial creation of a fragment.  This is called after
-   * {@link #onAttach (Activity)} and before
-   * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
-   * Note that this can be called while the fragment's activity is
-   * still in the process of being created.  As such, you can not rely
-   * on things like the activity's content divider_view_1 hierarchy being initialized
-   * at this point.  If you want to do work once the activity itself is
-   * created, see {@link #onActivityCreated(Bundle)}.
-   *
-   * @param savedInstanceState If the fragment is being re-created from a previous saved state, this
-   * is the state.
-   */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    /**
-     * There is no need to use savedInstanceState() because the movie list fetched is persisted
-     * / cached by the AsyncTaskLoader's subclass MovieListLoader upon configuration changes
-     */
-//    if ((savedInstanceState != null) && savedInstanceState.containsKey(MOVIE_LIST)) {
-//      // Let's get the saved movie list array from a saved state
-//      mCachedMovieList = savedInstanceState.getParcelableArrayList(MOVIE_LIST);
-//    }
-  }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-    mImageCloudOff = rootView.findViewById(R.id.image_main_no_connection);
-    mLoadingIndicator = rootView.findViewById(R.id.progressbar_main_base);
-    showLoadingIndicator();
-
-    // Get a reference to the RecyclerView (the movie grid), and attach the movie adapter to it.
-    mRvMovieGrid = rootView.findViewById(R.id.recycler_main_movie_grid);
-
-    // Al the items (movie posters) in the RecyclerView are the same size
-    mRvMovieGrid.setHasFixedSize(true);
-
-    // We will show the movie list in a grid with a parameterized number of columns
-    mRvMovieGrid.setLayoutManager(new GridLayoutManager(getContext(),
-        getResources().getInteger(R.integer.num_columns)));
-
-    /**
-     * The MovieAdapter will take data from a a JSON response from TheMovieDB.org
-     * server and use it to populate the grid it is attached to.
-     */
+  public void setupRecyclerViewAdapter() {
     // Set an empty adapter because the movies have not been fetched yet
     mMovieAdapter = new MovieAdapter(this);
     mRvMovieGrid.setAdapter(mMovieAdapter);
-
-    return rootView;
-  }
-
-  /**
-   * Called when the fragment's activity has been created and this
-   * fragment's hierarchy instantiated.  It can be used to do final
-   * initialization once these pieces are in place, such as retrieving
-   * views or restoring state.  It is also useful for fragments that use
-   * {@link #setRetainInstance(boolean)} to retain their instance,
-   * as this callback tells the fragment when it is fully associated with
-   * the new activity instance.  This is called after {@link #onCreateView}
-   * and before {@link #onViewStateRestored(Bundle)}.
-   *
-   * @param savedInstanceState If the fragment is being re-created from a previous saved state, this
-   * is the state.
-   */
-  @Override
-  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    getMovies();
-  }
-
-  protected void setSortingMethodParam(String sortingMethodParam) {
-    mSortingMethodParam = sortingMethodParam;
-  }
-
-  protected void getMovies() {
-    final Bundle bundle = new Bundle();
-    bundle.putString(EXTRA_MOVIE_SORTING_METHOD, mSortingMethodParam);
-
-//    LoaderManager lm = getLoaderManager();
-//    Loader<ArrayList<Movie>> loader = lm.getLoader(MOVIE_LIST_LOADER_ID);
-//    if (loader == null) {
-//      Log.d(LOG_TAG, "++++++++++ Initializing the loader");
-//      lm.initLoader(MOVIE_LIST_LOADER_ID, bundle, this);
-//    } else {
-//      // restartLoader() will start a new or restart an existing loader
-//      Log.d(LOG_TAG, "++++++++++ Restarting the loader");
-//      lm.restartLoader(MOVIE_LIST_LOADER_ID, bundle, this);
-//    }
-    // Show the progress bar
-//    showLoadingIndicator();
-
-    /**
-     * In this App, we want to persist and cache the movie list fetched from the Internet upon
-     * configuration changes (rotation of device) and in onPause() / onResume(). The persistence
-     * seems appropriate here because the data(movies and its values) more or less is not
-     * "real-time" data. So we need to use initLoader() instead of restartLoader() because
-     * initLoader() will reuse the last created loader if a loader with the specified ID already
-     * exists or will trigger onCreateLoader() if a loader with the specified ID does not exist yet.
-     */
-    getLoaderManager().initLoader(MOVIE_LIST_LOADER_ID, bundle, this);
-
   }
 
   /**
@@ -293,87 +143,42 @@ public class MovieListFragment
   }
 
   @Override
-  public void onMovieItemClick(int clickedItemIndex) {
-    MyPMErrorUtils.validateIndexInCollection(clickedItemIndex, mMovieList.size());
-    final Movie currentMovie = mMovieList.get(clickedItemIndex);
-    final Intent intent = new Intent(getActivity(),
-        MovieDetailActivity.class).putExtra(getString(R.string.EXTRA_SELECTED_MOVIE), currentMovie);
+  public void getMovies() {
+    final Bundle bundle = new Bundle();
+    bundle.putString(EXTRA_MOVIE_SORTING_METHOD, getSortingMethodParam());
 
-    startActivity(intent);
-  }
-
-  private void showLoadingIndicator() {
-    mLoadingIndicator.setVisibility(View.VISIBLE);
-  }
-
-  private void hideLoadingIndicator() {
-    mLoadingIndicator.setVisibility(View.INVISIBLE);
-  }
-
-  private void showErrorDisplay() {
-    mRvMovieGrid.setVisibility(View.INVISIBLE);
-    mImageCloudOff.setVisibility(View.VISIBLE);
-  }
-
-  public static class PopularMovieListFragment extends MovieListFragment {
-
-    private static final String SORT_BY_POPULARITY = "popularity.desc";
-
-    public PopularMovieListFragment() {
-      setSortingMethodParam(SORT_BY_POPULARITY);
-    }
-  }
-
-  public static class RatedMovieListFragment extends MovieListFragment {
-
-    private static final String SORT_BY_RATING = "vote_average.desc";
-
-    public RatedMovieListFragment() {
-      setSortingMethodParam(SORT_BY_RATING);
-    }
-  }
-
-  public static class FavoriteMovieListFragment extends MovieListFragment {
-
-    public FavoriteMovieListFragment() {
-            /*
-            We do not need to call setSortingMethodParam (""). In this case,
-            we are going to read from the local database and not to connect to TheMovieDB server
-            */
-    }
-
-    @Override
-    protected void getMovies() {
-      /*
-       * READ FROM DATABASE AND POPULATE "mCachedMovieList":
-       * if (Database is empty) {
-       *   show "No favorite movies"
-       * }
-       * else {
-       * display all movies in the database (we are storing only the FAVORITES movies
-       * in the database)
-       * }
-       */
-    }
-
+//    LoaderManager lm = getLoaderManager();
+//    Loader<ArrayList<Movie>> loader = lm.getLoader(MOVIE_LIST_LOADER_ID);
+//    if (loader == null) {
+//      Log.d(LOG_TAG, "++++++++++ Initializing the loader");
+//      lm.initLoader(MOVIE_LIST_LOADER_ID, bundle, this);
+//    } else {
+//      // restartLoader() will start a new or restart an existing loader
+//      Log.d(LOG_TAG, "++++++++++ Restarting the loader");
+//      lm.restartLoader(MOVIE_LIST_LOADER_ID, bundle, this);
+//    }
+    // Show the progress bar
+//    showLoadingIndicator();
 
     /**
-     * Called when the fragment is visible to the user and actively running.
-     * This is generally
-     * tied to {@link Activity#onResume() Activity.onResume} of the containing
-     * Activity's lifecycle.
+     * In this App, we want to persist and cache the movie list fetched from the Internet upon
+     * configuration changes (rotation of device) and in onPause() / onResume(). The persistence
+     * seems appropriate here because the data(movies and its values) more or less is not
+     * "real-time" data. So we need to use initLoader() instead of restartLoader() because
+     * initLoader() will reuse the last created loader if a loader with the specified ID already
+     * exists or will trigger onCreateLoader() if a loader with the specified ID does not exist yet.
      */
-    @Override
-    public void onResume() {
-      super.onResume();
-      // Re-query the DB after the Fragment/Activity has been -paused-: Any time we leave the
-      // MainActivity / MovieListFragment and return(when we go and get back from
-      // MovieDetailActivity / MovieDetailFragment where the selected movie could have been marked
-      // as favourite / not favourite
+    getLoaderManager().initLoader(MOVIE_LIST_LOADER_ID, bundle, this);
+  }
 
-      // restart the loader!
+  @Override
+  public Movie getSelectedMovie(int clickedItemIndex) {
+    return mMovieList.get(clickedItemIndex);
+  }
 
-    }
+  @Override
+  public int movieListSize() {
+    return mMovieList.size();
   }
 
   private static class MovieListLoader extends AsyncTaskLoader<ArrayList<Movie>> {
@@ -386,8 +191,12 @@ public class MovieListFragment
     MovieListLoader(Context context, Bundle args) {
       super(context);
       mArgs = args;
-      mCachedMovieList = new ArrayList<>(MovieListFragment.MOVIES_RESULT_SIZE);
+      mCachedMovieList = new ArrayList<>(com.ampie_guillermo.popularmovies.ui.MovieListFragment.MOVIES_RESULT_SIZE);
       mUiError = new UiErrorHelper();
+    }
+
+    UiErrorHelper getUiError() {
+      return mUiError;
     }
 
     @Nullable
@@ -600,9 +409,23 @@ public class MovieListFragment
       }
       return new ArrayList<>(Arrays.asList(movieList));
     }
+  }
 
-    UiErrorHelper getUiError() {
-      return mUiError;
+  public static class PopularMovieListFragment extends MovieListFragment {
+
+    private static final String SORT_BY_POPULARITY = "popularity.desc";
+
+    public PopularMovieListFragment() {
+      setSortingMethodParam(SORT_BY_POPULARITY);
+    }
+  }
+
+  public static class RatedMovieListFragment extends MovieListFragment {
+
+    private static final String SORT_BY_RATING = "vote_average.desc";
+
+    public RatedMovieListFragment() {
+      setSortingMethodParam(SORT_BY_RATING);
     }
   }
 }
